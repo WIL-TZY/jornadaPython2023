04ec75dab138a334ab97fb74b0382d6f6f6a2a6b
willowitzy <willowitzy@gmail.com>
2023-10-01 16:38:40 -0300

    criando túnel de comunicação -> pubsub.subscribe()

--- websiteChatroom/main.py
+++ websiteChatroom/main.py
@@ -15,32 +15,84 @@ import flet as ft
 # FLET-PASSO-2: Modelo de execução do Flet --- Função main() deverá receber a página web principal como parâmetro
 def main(pagina):
     texto = ft.Text("Chatroom")
-    nome_usuario = ft.TextField()
+    chat = ft.Column()
+    campo_usuario = ft.TextField(label="Escreva seu nome")
+    campo_mensagem = ft.TextField(label="Digite uma mensagem")
 
-    # ETAPA2: Criar o Pop Up para inserir o nome de usuário e entrar no chat (FRONT)
+    # Adicionar o chat na página
+    pagina.add(chat)
+
+    # --------------------------------------- BACK END ---------------------------------------#
+    # ETAPA 4: Quando entrar no chat, o usuário pode enviar e receber mensagens na intranet (BACK) 
+    # A mensagem sinalizando que o usuário entrou no chat
+    def enviar_mensagem_tunel(mensagem):
+        # Adicionar a mensagem no chat p/ todos verem
+        chat.controls.append(ft.Text(mensagem))
+
+        pagina.update() # Recarrega a página (necessário para que mudanças ocorram na página)
+
+
+    # Criar o túnel de conexão PUB-SUB (Publish & Subscribe)
+    # subscribe() é responsável por transmitir essa mensagem a todos que estão conectados na intranet
+    pagina.pubsub.subscribe(enviar_mensagem_tunel)
+
+    # Ocorre quando o botão "Enviar" é clicado
+    def enviar_mensagem(evento):
+        # Envia para o túnel a mensagem que está armazenada no campo
+        pagina.pubsub.send_all(campo_mensagem.value)
+        # Limpar o campo de mensagem
+        campo_mensagem.value = ""
+        pagina.update() # Recarrega a página
+    # -----------------------------------------------------------------------------------------#
+
+    botao_enviar = ft.ElevatedButton(text="Enviar", on_click=enviar_mensagem)
+    
+    # ETAPA 3: Criação do chat (FRONT)
+    # O campo e o botão de enviar mensagem devem ser criados
+    def fechar_popup(evento):
+        # Fechar o Popup
+        popup.open = False
+        
+        # Remover o título e o botão "Iniciar Chat"
+        pagina.remove(texto)
+        pagina.remove(botao_iniciar)
+
+        pagina.add(ft.Row(      # Inline
+            [       
+                campo_mensagem, # Criar o campo de mensagem do usuário
+                botao_enviar    # Criar o botão de "Enviar Mensagem"
+            ]
+        ))
+
+        pagina.update() # Recarrega a página
+
+
+    # ETAPA 2: Criação do Pop Up para inserir o nome de usuário e entrar no chat (FRONT)
     popup = ft.AlertDialog(
-        title=ft.Text("Bem vindo(a) ao chat"),          # A parte superior da caixa (como um "header")
-        modal=True,                                     # shape do popup
-        content=nome_usuario,                           # O corpo da caixa
-        actions=[ft.ElevatedButton(text="Entrar")],     # A parte inferior da caixa
-        on_dismiss=lambda e: print("Popup finalizado")
+        title=ft.Text("Bem vindo(a) ao chat"),                                  # A parte superior da caixa (acima do content)
+        modal=True,     # Modal=True --> Não dá pra sair do popup clicando fora. Modal=False, é possível ignorar a janelinha clicando em outro ponto da página.
+        content=campo_usuario,                                                  # O corpo da caixa
+        actions=[ft.ElevatedButton(text="Entrar", on_click=fechar_popup)],      # A parte inferior da caixa (abaixo do content)
+        on_dismiss=lambda e: print("Chat inicializado")
     )
 
     def entrar_chat(evento): # ---> "Event Handler" (similar ao JS)
+        # Chama o pop up
         pagina.dialog = popup
         popup.open = True
-        pagina.update() # Recarrega a página
 
         join_txt = ft.Text("Entrou no chat")
         pagina.add(join_txt)
+        pagina.update() # Recarrega a página
 
-    # ETAPA1: Botão "Iniciar Chat" (FRONT)
+    # ETAPA 1: Botão "Iniciar Chat" (FRONT)
     botao_iniciar = ft.ElevatedButton(text="Iniciar chat", on_click=entrar_chat)
 
 
 
-    # -------------------- DESENHAR NA PÁGINA ----------------------- #
+    # -------------------- DESENHAR NA PÁGINA (ESTÁTICA) ----------------------- #
     # Para adicionar um item na página, é necessário usar a função .add() 
+    # O que estiver aqui é o que é desenhado na tela logo que a página é iniciada pela primeira vez
     pagina.add(
         texto,
         botao_iniciar
@@ -60,10 +112,5 @@ ft.app(target=main, view=ft.WEB_BROWSER) # Padrão: view=AppView
 
 
 
-
-# ETAPA3: Quando entrar no chat: (BACK) 
-    # A mensagem sinalizando que o usuário entrou no chat
-    # O campo e o botão de enviar mensagem devem ser criados
-
-# ETAPA4: A cada mensagem enviada: (BACK) 
+# ETAPA 5: A cada mensagem enviada: (BACK) 
     # Nome: {Texto da mensagem}
