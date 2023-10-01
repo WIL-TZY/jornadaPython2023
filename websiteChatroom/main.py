dd11785aaa3e2c6e616d00d2ac28204b6568ae0e
willowitzy <willowitzy@gmail.com>
2023-10-01 14:15:15 -0300

    pop up modal, botões + funcionalidades básicas

--- websiteChatroom/main.py
+++ websiteChatroom/main.py
@@ -15,14 +15,36 @@ import flet as ft
 # FLET-PASSO-2: Modelo de execução do Flet --- Função main() deverá receber a página web principal como parâmetro
 def main(pagina):
     texto = ft.Text("Chatroom")
-    novo_texto = ft.Text("Novo texto")
-    # botao = ft.IconButton()
-    # alerta = ft.AlertDialog()
+    nome_usuario = ft.TextField()
+
+    # ETAPA2: Criar o Pop Up para inserir o nome de usuário e entrar no chat (FRONT)
+    popup = ft.AlertDialog(
+        title=ft.Text("Bem vindo(a) ao chat"),          # A parte superior da caixa (como um "header")
+        modal=True,                                     # shape do popup
+        content=nome_usuario,                           # O corpo da caixa
+        actions=[ft.ElevatedButton(text="Entrar")],     # A parte inferior da caixa
+        on_dismiss=lambda e: print("Popup finalizado")
+    )
+
+    def entrar_chat(evento): # ---> "Event Handler" (similar ao JS)
+        pagina.dialog = popup
+        popup.open = True
+        pagina.update() # Recarrega a página
+
+        join_txt = ft.Text("Entrou no chat")
+        pagina.add(join_txt)
+
+    # ETAPA1: Botão "Iniciar Chat" (FRONT)
+    botao_iniciar = ft.ElevatedButton(text="Iniciar chat", on_click=entrar_chat)
 
 
+
+    # -------------------- DESENHAR NA PÁGINA ----------------------- #
     # Para adicionar um item na página, é necessário usar a função .add() 
-    pagina.add(texto)
-    pagina.add(novo_texto)
+    pagina.add(
+        texto,
+        botao_iniciar
+    )
 
 
 # Cada página será uma função diferente
@@ -32,13 +54,12 @@ def pagina2():
 # FLET-PASSO-3: Função app() Sinaliza qual é a página inicial do app
 # É necessário passar a página principal para essa função usando o parâmetro 'target'
 ft.app(target=main, view=ft.WEB_BROWSER) # Padrão: view=AppView
-# Modificando o parâmetro view para ft.WEB_BROWSER possibilita visualizar o app como site
+# Modificando o parâmetro view para 'view=ft.WEB_BROWSER' possibilita visualizar o app como site
 # Endereço: localhost:53933
 
 
-# ETAPA1: Botão "Iniciar Chat" (FRONT)
 
-# ETAPA2: Pop Up para inserir o nome de usuário e entrar no chat (FRONT)
+
 
 # ETAPA3: Quando entrar no chat: (BACK) 
     # A mensagem sinalizando que o usuário entrou no chat
